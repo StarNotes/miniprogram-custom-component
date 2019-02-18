330c4c9ea0a4c112286357cf1553dd1bff7d54e0
xuyuexing <xuyuexing@juqitech.com>
2019-02-18 14:24:22 +0800

    observer 数据校验

--- src/index.js
+++ src/index.js
@@ -3,7 +3,42 @@ Component({
   properties: {
     dataList: {// 属性名
       type: Array, // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
-      value: []// 属性初始值（可选），如果未指定则会根据类型选择一个
+      value: [],// 属性初始值（可选），如果未指定则会根据类型选择一个
+      observer: function (newVal, oldVal){// 参数校验
+				if (!newVal || !newVal.length){
+					console.log("请传入正确的值");
+				} else {
+					newVal.forEach((item)=>{
+						if (!item.imageUrl){
+							item.imageUrl = "/pages/images/2.jpg";
+						}
+						if (item.EType == "toPage" && !item.path){
+							console.log("请输入跳转路径");
+							item.path = "../index/index";
+						} else if (item.EType == "toMiniprogram"){
+							if (!item.appid){
+								console.log("当EType为'toMiniprogram'时,请输入appid");
+								item.EType = "";
+							} else if(!item.path){
+								console.log("请输入跳转路径");
+								item.path = "../index/index";
+							}
+						} else if (item.EType == 'link'){
+							if (!item.link) {
+								console.log("当EType为'link'时,link为必传");
+							} 
+						} else if (item.EType == 'function') {
+							if (!item.func) {
+								console.log("当EType为'function'时,func为必传");
+							}
+						}
+						return item;
+					})
+				}
+				this.setData({
+					dataList: newVal
+				})
+			}
     },
   },
   data: {
